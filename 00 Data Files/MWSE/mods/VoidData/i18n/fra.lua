76963dc1cd6d189493235e429f0396ee81c33d10
Kynesifnar <kynesifnar@gmail.com>
2025-10-16 23:27:47 -0400

    Added fra.lua

---
+++ 00 Data Files/MWSE/mods/VoidData/i18n/fra.lua
@@ -0,0 +1,21 @@
+return {
+	-- Magic Text
+	["magic.summonFarmer"] = "Appel de cultivateur",
+	["magic.summonFarmerDesc"] = "Cet effet permet d'invoquer un cultivateur des Royaumes extérieurs. Il apparaît à 2 mètres du lanceur et attaque toute entité hostile à son maître jusqu'à ce que le sort prenne fin ou qu'il soit tué. Quand il meurt ou quand le sort ne fait plus effet, il disparaît et retourne dans les Royaumes extérieurs.",
+
+	-- MCM Text
+	["mcm.name"] = "Ressources communes du Vide",
+	["mcm.preferences"] = "Préférences",
+	["mcm.preferencesInfo"] = "Ressources communes du Vide MWSE-Lua v1.0",
+	["mcm.credits"] = "Crédits :",
+	["mcm.Kynesifnar"] = "Kynesifnar - Scripting",
+	["mcm.settings"] = "Paramètres",
+
+	["mcm.skyboxesLabel"] = "Cieux alternatifs",
+	["mcm.skyboxesDescription"] = "Ajout des cieux pour les différents royaumes au-delà de Nirn.\nRequiert un rechargement.\n\nPar défaut : activé\n\n",
+	["mcm.newEffectsLabel"] = "Nouveaux effets magiques",
+	["mcm.newEffectsDescription"] = "Implémente de nouveaux effets magiques propres à Ressources communes du Vide, comme des effets permettant d'invoquer de nouveaux types de Daedras.\nRequiert un redémarrage.\n\nPar défaut : activé\n\n",
+	["mcm.sdfLabel"] = "Champ de déclin statique",
+	["mcm.sdfDescription"] = "Implémente une mécanique représentant les effets délétères d'un séjour sur les lunes ainsi que de l'équipement permettant de lutter contre.\nRequiert un rechargement.\n\nPar défaut : activé\n\n",
+
+}
